863437a1f5115e47b82c09373df16e35c0fa8a35
AmzAust <austin_balogh@yahoo.com>
2020-06-30 21:45:47 -0400

    began backend implementation

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://austin:austinpw@cluster0.4tsp9.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
---
+++ server/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const bcrypt = require("bcryptjs");
+
+router.get("/", (req, res) => {
+  res.send("hello");
+});
+
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "add all required fields" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (savedUser) {
+      return res
+        .status(422)
+        .json({ error: "user already exists with this email" });
+    }
+    bcrypt.hash(password, 12).then((hashedpassword) => {
+      const user = new User({
+        email,
+        password: hashedpassword,
+        name,
+      });
+      user
+        .save()
+        .then((user) => {
+          res.json({ message: "saved successfully" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  });
+});
+
+module.exports = router;
