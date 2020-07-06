2cfce16b626936b1e82c9f1aa91df580c75ee4aa
AmzAust <austin_balogh@yahoo.com>
2020-07-05 23:08:47 -0400

    added homepage image fetching and logout feature

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../App";
 
 const Navbar = () => {
   const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
   const renderList = () => {
     if (state) {
       return [
@@ -12,6 +13,18 @@ const Navbar = () => {
         </li>,
         <li>
           <Link to="/create">Create Post</Link>
+        </li>,
+        <li>
+          <button
+            className="btn #81d4fa light-blue lighten-3"
+            onClick={() => {
+              localStorage.clear();
+              dispatch({ type: "CLEAR" });
+              history.push("/signin");
+            }}
+          >
+            Logout
+          </button>
         </li>,
       ];
     } else {
